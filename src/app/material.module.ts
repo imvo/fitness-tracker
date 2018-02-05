49c8e7f7bd9bc054348b480cf582b088fd069e26
vmalakhov <wasiliy123@ya.ru>
2018-02-05 23:24:39 +0300

    finalize signup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule} from '@angular/material';
+import {
+  MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
+  MatNativeDateModule, MatCheckboxModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 })
 export class MaterialModule {}
