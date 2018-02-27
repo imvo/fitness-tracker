52c10ba415f5d9c1e266e73856d1c14576604095
vmalakhov <wasiliy123@ya.ru>
2018-02-27 20:20:57 +0300

    add sidebar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,31 @@
 import {NgModule} from '@angular/core';
 import {
   MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule,
-  MatNativeDateModule, MatCheckboxModule
+  MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
-  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ]
 })
 export class MaterialModule {}
